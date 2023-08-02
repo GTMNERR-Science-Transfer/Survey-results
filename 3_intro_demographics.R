31007db37bec9509852f8a15f763ab86d34d7813
Geraldine Klarenberg <gklarenberg@users.noreply.github.com>
2023-08-01 21:57:40 -0400

    Updated script 3 on demographics (still has an error in plot on engagement). Created 3 jpegs (age, engagement, access data)

--- 3_intro_demographics.R
+++ 3_intro_demographics.R
@@ -74,18 +74,17 @@ ggplot(intro %>%
 
 ggsave("results/intro_connected.jpg", width = 12, height = 7)
 
-engage_classes <- mc_text_lookup %>% filter(ImportId == "QID7") %>% pull(q_text)
-engage <- data.frame(choiceId=c(1,2,3,4,5,6,7), engage_classes)
-engage$engage_classes <- factor(engage$engage_classes, 
-                                levels = c("Daily", "At least once a week",
-                                           "2-3 times a month", "Once a month",
-                                           "Once every 6 months", "Once every year",
-                                           "Less than once a year"))
-# ORDER the 
-ggplot(intro %>% 
-         filter(qname == "D-2"), # %>% 
-         #rename(choiceId = answer, qname = question) %>% 
-         #left_join(engage),
+##### How often do you engage with the GTM? -----------------------------------
+# To show the order of the options here properly, we need to re-order the options
+intro_engage <- intro %>% 
+  filter(qname == "D-2") %>% 
+  mutate(q_text = factor(q_text, levels = c("Daily", "At least once a week",
+                                            "2-3 times a month", "Once a month",
+                                            "Once every 6 months", "Once every year",
+                                            "Less than once a year")))
+# Plot
+ggplot(intro_engage %>% 
+         filter(qname == "D-2"), 
        aes(y = fct_rev(q_text), fill = q_text))+
   geom_bar() +
   labs(x = "", y = "", title = "How often do you engage with the GTM?") +
@@ -107,7 +106,29 @@ ggplot(intro %>%
   geom_text(data = intro %>% 
              filter(str_detect(qname, "D-3")) %>% 
              group_by(q_text) %>% summarize(ave_rank = mean(order, na.rm = TRUE)),
-           aes(x = 5, y = q_text, label = ave_rank))+
+           aes(x = 5, y = q_text, label = ave_rank))+ ######## 07/17/2023 LEFT OFF HERE: error with 2nd layer and unique()
+  theme_bw()+
+  theme(axis.text.y = element_text(size=15))
+
+test1 <- intro %>% 
+  filter(str_detect(qname, "D-3")) %>% 
+  group_by(q_text) %>% summarize(ave_rank = mean(order, na.rm = TRUE),
+                                 med_rank = median(order, na.rm = TRUE)) %>% ### NOT WORKING
+  mutate(q_text = factor(q_text)) %>% 
+  mutate(loc = 5) 
+ggplot(data = test1, aes(x = ave_rank, y = fct_rev(fct_reorder(q_text, ave_rank))))+
+  geom_col()
+
+ggplot(intro %>% 
+         filter(str_detect(qname, "D-3")),
+       aes(y = fct_rev(fct_infreq(q_text)), fill = factor(order)))+
+  geom_bar() +
+  labs(y = "", x = "", title = "What datasets are you interested in?") +
+  scale_x_continuous(breaks = function(x) seq(ceiling(x[1]), floor(x[2]), by = 1))+
+  scale_fill_viridis_d(option = "plasma", name = "Rank")+
+  # add annotation with average score
+  #geom_text(data = test1,
+  #          aes(x = 5, y = q_text, label = q_text))+ ######## 07/17/2023 LEFT OFF HERE: error with 2nd layer and unique()
   theme_bw()+
   theme(axis.text.y = element_text(size=15))
 
