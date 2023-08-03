a4632a198ba7ed41f7b578912173a1c143cc885f
Geraldine Klarenberg <gklarenberg@users.noreply.github.com>
2023-08-03 16:07:09 -0400

    Finalized figures for dashboard questions. Added figures that show average ranking.

--- 6_dashboard.R
+++ 6_dashboard.R
@@ -62,7 +62,19 @@ ggplot(dashboard %>%
   theme_bw()+
   theme(axis.text.y = element_text(size=12))
 
-ggsave("results/dashboard_form.jpg", width = 12, height = 7)
+ggsave("results/dashboard_form1.jpg", width = 12, height = 7)
+
+# Similar plot, but with the average ranks (1 = most interested)
+ggplot(dashboard %>% 
+         filter(qname_main == "T-3") %>% 
+         group_by(q_text) %>% summarize(ave_rank = mean(order, na.rm = TRUE)) %>% 
+         mutate(q_text = fct_reorder(q_text, ave_rank)),
+       aes(x = ave_rank, y = fct_rev(q_text))) +
+  geom_col(fill = "royalblue")+
+  labs(y = "", x = "", title = "What format of data delivery would suit your needs best?\nAverage ranking (1 = most interested in)")+
+  theme_bw()+
+  theme(axis.text.y = element_text(size=15))
+ggsave("results/dashboard_form2.jpg", width = 12, height = 7)
 
 ##### T-4 Data delivery? (QID24) ----------------------------------------------
 
@@ -70,10 +82,22 @@ ggplot(dashboard %>%
          filter(qname_main == "T-4"),
        aes(y = fct_rev(fct_infreq(q_text)), fill = factor(order)))+
   geom_bar() +
-  labs(y = "", x = "", title = "What form of information do you prefer?") +
+  labs(y = "", x = "", title = "What format of data delivery would suit your needs best?") +
   #scale_x_continuous(breaks = function(x) seq(ceiling(x[1]), floor(x[2]), by = 1))+
   scale_fill_viridis_d(option = "rocket", name = "Rank")+
   theme_bw()+
   theme(axis.text.y = element_text(size=12))
 
-ggsave("results/dashboard_delivery.jpg", width = 12, height = 7)
+ggsave("results/dashboard_delivery1.jpg", width = 12, height = 7)
+
+# Similar plot, but with the average ranks (1 = most interested)
+ggplot(dashboard %>% 
+         filter(qname_main == "T-4") %>% 
+         group_by(q_text) %>% summarize(ave_rank = mean(order, na.rm = TRUE)) %>% 
+         mutate(q_text = fct_reorder(q_text, ave_rank)),
+       aes(x = ave_rank, y = fct_rev(q_text))) +
+  geom_col(fill = "royalblue")+
+  labs(y = "", x = "", title = "What format of data delivery would suit your needs best?\nAverage ranking (1 = most interested in)")+
+  theme_bw()+
+  theme(axis.text.y = element_text(size=15))
+ggsave("results/dashboard_delivery2.jpg", width = 12, height = 7)
